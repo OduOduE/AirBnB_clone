9d9aa4c4a0412cdcc85756d7d1a190527feec3ac
Odu, Odu <theoduemmanuel@gmail.com>
2023-02-12 15:29:44 +0000

    console

--- models/base_model.py
+++ models/base_model.py
@@ -46,12 +46,12 @@ class BaseModel:
         - only instance attributes will be returned
         - a key __class__ is added to class name of the object
         - created_at and updated_at must be converted to string object in ISO
-        object
+        format
         """
         dict_1 = self.__dict__.copy()
         dict_1["__class__"] = self.__class__.__name__
         for k, v in self.__dict__.items():
             if k in ("created_at", "updated_at"):
-                v = slef.__dict__[k].isoformat()
+                v = self.__dict__[k].isoformat()
                 dict_1 = v
         return dict_1
